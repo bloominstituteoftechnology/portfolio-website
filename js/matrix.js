f1dc0f45fb10ceaa319e2bd2269f4a03b56fe9d3
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-01-28 15:26:00 -0500

    update

--- js/matrix.js
+++ js/matrix.js
@@ -1,9 +1,8 @@
 
 var s = window.screen;
-var width = q.width = s.width;
-var height = q.height = s.height;
+var width = q.width;
+var height = q.height;
 var letters = Array(256).join(1).split('');
-
 var draw = function () {
   q.getContext('2d').fillStyle='rgba(255,255,255,.2)';
   q.getContext('2d').fillRect(0,0,width,height);
@@ -13,6 +12,7 @@ var draw = function () {
     x_pos = index * 70;
     q.getContext('2d').fillText(text, x_pos, y_pos);
     letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+    
   });
 };
 setInterval(draw, 55);
