fdd1b2305112422ae0dd5973b053fee5b1837af7
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-01-29 11:54:43 -0500

    matrix code setup complete

---
+++ js/events.js
@@ -0,0 +1,4 @@
+let button = document.getElementById("about-btn");
+    button.addEventListener('click', () => {
+       alert("button clicked");
+    });
--- js/matrix.js
+++ js/matrix.js
@@ -1,18 +1,35 @@
 
-var s = window.screen;
-var width = q.width;
-var height = q.height;
-var letters = Array(256).join(1).split('');
-var draw = function () {
-  q.getContext('2d').fillStyle='rgba(255,255,255,.2)';
-  q.getContext('2d').fillRect(0,0,width,height);
-  q.getContext('2d').fillStyle='rgba(100,100,200,.5)';
-  letters.map(function(y_pos, index){
-    text = String.fromCharCode(3e4+Math.random()*77);
-    x_pos = index * 70;
-    q.getContext('2d').fillText(text, x_pos, y_pos);
-    letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
-    
-  });
-};
-setInterval(draw, 55);
+
+
+  var s = document.documentElement.clientWidth;
+  var s2 = document.documentElement.clientHeight;
+  var width = q.width = s - 30;
+  var height = q.height = s2;
+  var letters = Array(256).join(1).split('');
+  var draw = function () {
+    q.getContext('2d').fillStyle='rgba(255,255,255,.1)';
+    q.getContext('2d').fillRect(0,0,width,height);
+    q.getContext('2d').fillStyle='rgba(0,100,200,.8)';
+    letters.map(function(y_pos, index){
+      text = String.fromCharCode(3e4+Math.random()*33);
+      x_pos = index * 33;
+      q.getContext('2d').fillText(text, x_pos, y_pos);
+      letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+      
+    });
+  };
+
+ setInterval(draw, 70);
+console.log(s);
+console.log(width);
+
+window.onresize = function(){ location.reload(); }
+
+
+
+//=====events
+
+let button = document.getElementById("about-btn");
+    button.addEventListener('click', () => {
+       alert("button clicked");
+    });
