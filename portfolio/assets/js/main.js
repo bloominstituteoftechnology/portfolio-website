8f2eda447dd0c210324d2e7b64ea0f7183c744e0
Joseph Luna <josephluna@Josephs-MBP.local>
2019-04-16 11:45:31 -0500

    revised overlay with greensock, updated border and padding in overlay, add LinkedIn profile to menu

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -98,7 +98,10 @@ overlay.id = "overlay";
 
 document.querySelector('body').append(overlay);
 
+
+
 const clickForOverlay = document.querySelector('.image.profile'); // Profile Image In Banner
+
 
 clickForOverlay.addEventListener('click', (event)=> {
 			on();
@@ -110,12 +113,20 @@ clickForOverlay.addEventListener('click', (event)=> {
 
 function on(){
 	const overlay = document.querySelector("#overlay")
+	TweenMax.to(overlay,1.5,{opacity:1})
 	overlay.style.display = "block";
 }
 
 function off(){
 	const overlay = document.querySelector("#overlay");
-	overlay.style.display = "none"
+	TweenMax.to(overlay,1,{opacity:0})
+	const time = setInterval(dimissOverlay,1000)
+	time
+	function dimissOverlay(){
+	 overlay.style.display = "none";
+	 clearInterval(time)
+	}
+	
 }
 
 // End Overlay
@@ -133,6 +144,10 @@ const personalProfileInfo = document.createElement('p');
 personalProfileInfo.className = "personal-info content";
 personalProfileInfo.textContent = "Hi, my name is Joseph Luna! Born and raised here in the great state of Texas, I’m on an exciting journey to build upon my career as a Full Stack Web Developer. I have always been interested in computers and have spent a better part of my time learning and developing skills to effectively contribute to the Technology Industry. My skill sets in language and development methodologies are a mixture of formal and self-taught with a fiery ambition to learn more and keep on evolving my knowledge. It has always been intriging me how technology can be so helpful in our everyday lives and how yet, there are so many ways left to integrate useful software to make our personal and professional lifestyles easier.  Communication is the life line of business and the need to have a website or SPA  that is visually appealing, intuitive, and reactive is just as important as a brick and mortar store. I too, have been an entrepreneur and understand the necessity of trafficking customers to something that represents the culture of your business. This is my driving force this symbiosis of user interactions with company products and services while making the user experience enjoyable. That’s what I’m about and that’s why I’m here. I would like to help make a make a difference for you and your company too. Just let me know how I can be of assistance and I promise you I'll bring my A game, C game, and JavaScript and well, you get the idea! Thanks for reading and have a great day! "
 overlay.append(personalProfileInfo)
+personalProfileInfo.style.border = "2px solid white";
+personalProfileInfo.style.borderRadius = "8%";
+personalProfileInfo.style.padding = "2%";
+personalProfileInfo.style.lineHeight = "1.5";
 
 //tooltip
 
@@ -145,7 +160,6 @@ document.addEventListener('scroll', (event)=>{
 	window.setInterval(function (){
 			if(counter < 2000){
 			counter+=1;
-			console.log(counter);
 			showToolTipOn();
 			}
 			else if(counter >=2000){
