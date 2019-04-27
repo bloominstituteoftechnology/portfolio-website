e318d517f757902df4a3a076bfb416eb122f5295
brellin <cekakid@gmail.com>
2019-04-26 23:23:28 -0500

    contact media query and attemp form

--- portfolio/src/components/Contact/Contact.js
+++ portfolio/src/components/Contact/Contact.js
@@ -2,21 +2,22 @@ import React from 'react'
 import './contact.scss'
 
 const Contact = props => {
+
     return (
         <div className='contact'>
             <h1>Contact</h1>
             <form method='post' name='contact'>
                 <input type='hidden' name='contact' value='contact' />
                 <div className="info">
-                    <label for="name">Name</label>
+                    <label htmlFor="name">Name</label>
                     <input type="text" name="name" id="name" placeholder='John' />
                 </div>
                 <div className="info">
-                    <label for="email">Email</label>
-                    <input type="email" required='true' name="email" id="email" placeholder='someone@something.com' />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" required={true} name="email" id="email" placeholder='someone@something.com' />
                 </div>
                 <div className="message">
-                    <label for="message">Message</label>
+                    <label htmlFor="message">Message</label>
                     <textarea name="message" id="message" rows="4" placeholder='Hey, I really liked your site and wanted to leave some feedback for you!' />
                 </div>
                 <div className='submit'>
