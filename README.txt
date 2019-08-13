cc24cbc6142a7ad8fa6971564a7692a5428fa024
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-08-13 13:59:43 -0400

    new site is going live

---
+++ README.txt
@@ -0,0 +1,1 @@
+POrtfolio Website for Ethan Baugehr
---
+++ assets/js/matrix.js
@@ -0,0 +1,22 @@
+
+  var s = document.documentElement.clientWidth;
+  var s2 = document.documentElement.clientHeight;
+  var width = q.width = s - 30;
+  var height = q.height = s2;
+  var letters = Array(256).join(1).split('');
+  var draw = function () {
+    q.getContext('2d').fillStyle='rgba(255,255,255,.1)';
+    q.getContext('2d').fillRect(0,0,width,height);
+    q.getContext('2d').fillStyle='#323538';
+    letters.map(function(y_pos, index){
+      text = String.fromCharCode(3e4+Math.random()*33);
+      x_pos = index * 22;
+      q.getContext('2d').fillText(text, x_pos, y_pos);
+      letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+
+    });
+  };
+
+ setInterval(draw, 70);
+
+window.onresize = function(){ location.reload(); }
