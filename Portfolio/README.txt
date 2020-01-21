a30d3857a4ee9d201c40a191244ba1a8b4b74d38
Sam Ardis <ardis.productions@gmail.com>
2020-01-21 15:26:56 -0500

    Added new portfolio sight and deleted the old one

--- Portfolio/README.txt
+++ Portfolio/README.txt
@@ -1,21 +1,17 @@
-Landed by HTML5 UP
+Solid State by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-A dark, slick, modern, responsive, adjective-drenched design built around an extremely
-dynamic landing page (scroll that mofo!). Inspired by Big Picture, another design
-of mine with a similarish feel/flow, only this time I took it waaaaaay further and
-actually made it multipurpose (versus copping out and making it a one pager like I
-did last time ;) Includes multiple pages, a bunch of pre-styled elements, and all
-its Sass sources.
+After a somewhat extended break from HTML5 UP (to work on a secret-ish new project --
+more on that later!) I'm back with a brand new design: Solid State, a slick new multi-
+pager that combines some of the ideas I've played with over at Pixelarity with an "angular"
+sort of look. Hope you dig it :)
 
 Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
 you can use for pretty much whatever.
 
-(* = Not included)
-
-Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
+(* = not included)
 
 AJ
 aj@lkn.io | @ajlkn
--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Landed by HTML5 UP
+	Solid State by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,15 +7,17 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body');
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
 		});
 
 	// Play initial animations on page load.
@@ -25,226 +27,119 @@
 			}, 100);
 		});
 
-	// Touch mode.
-		if (browser.mobile)
-			$body.addClass('is-touch');
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
 
-	// Scrolly links.
-		$('.scrolly').scrolly({
-			speed: 2000
-		});
+			$window.on('resize', function() { $window.trigger('scroll'); });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			alignment: 'right',
-			hideDelay: 350
-		});
-
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
-	// Parallax.
-	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
-		if (browser.name == 'ie'
-		||	browser.mobile) {
-
-			$.fn._parallax = function() {
-
-				return $(this);
-
-			};
-
-		}
-		else {
-
-			$.fn._parallax = function() {
-
-				$(this).each(function() {
-
-					var $this = $(this),
-						on, off;
-
-					on = function() {
-
-						$this
-							.css('background-position', 'center 0px');
-
-						$window
-							.on('scroll._parallax', function() {
-
-								var pos = parseInt($window.scrollTop()) - parseInt($this.position().top);
-
-								$this.css('background-position', 'center ' + (pos * -0.15) + 'px');
-
-							});
-
-					};
-
-					off = function() {
-
-						$this
-							.css('background-position', '');
-
-						$window
-							.off('scroll._parallax');
-
-					};
-
-					breakpoints.on('<=medium', off);
-					breakpoints.on('>medium', on);
-
-				});
-
-				return $(this);
-
-			};
-
-			$window
-				.on('load resize', function() {
-					$window.trigger('scroll');
-				});
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
 
 		}
 
-	// Spotlights.
-		var $spotlights = $('.spotlight');
+	// Menu.
+		var $menu = $('#menu');
 
-		$spotlights
-			._parallax()
-			.each(function() {
+		$menu._locked = false;
 
-				var $this = $(this),
-					on, off;
+		$menu._lock = function() {
 
-				on = function() {
+			if ($menu._locked)
+				return false;
 
-					var top, bottom, mode;
+			$menu._locked = true;
 
-					// Use main <img>'s src as this spotlight's background.
-						$this.css('background-image', 'url("' + $this.find('.image.main > img').attr('src') + '")');
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
 
-					// Side-specific scrollex tweaks.
-						if ($this.hasClass('top')) {
+			return true;
 
-							mode = 'top';
-							top = '-20%';
-							bottom = 0;
+		};
 
-						}
-						else if ($this.hasClass('bottom')) {
+		$menu._show = function() {
 
-							mode = 'bottom-only';
-							top = 0;
-							bottom = '20%';
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
 
-						}
-						else {
+		};
 
-							mode = 'middle';
-							top = 0;
-							bottom = 0;
+		$menu._hide = function() {
 
-						}
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
 
-					// Add scrollex.
-						$this.scrollex({
-							mode:		mode,
-							top:		top,
-							bottom:		bottom,
-							initialize:	function(t) { $this.addClass('inactive'); },
-							terminate:	function(t) { $this.removeClass('inactive'); },
-							enter:		function(t) { $this.removeClass('inactive'); },
+		};
 
-							// Uncomment the line below to "rewind" when this spotlight scrolls out of view.
+		$menu._toggle = function() {
 
-							//leave:	function(t) { $this.addClass('inactive'); },
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
 
-						});
+		};
 
-				};
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
 
-				off = function() {
+				event.stopPropagation();
 
-					// Clear spotlight's background.
-						$this.css('background-image', '');
+				// Hide.
+					$menu._hide();
 
-					// Remove scrollex.
-						$this.unscrollex();
+			})
+			.find('.inner')
+				.on('click', '.close', function(event) {
 
-				};
+					event.preventDefault();
+					event.stopPropagation();
+					event.stopImmediatePropagation();
 
-				breakpoints.on('<=medium', off);
-				breakpoints.on('>medium', on);
+					// Hide.
+						$menu._hide();
 
-			});
+				})
+				.on('click', function(event) {
+					event.stopPropagation();
+				})
+				.on('click', 'a', function(event) {
 
-	// Wrappers.
-		var $wrappers = $('.wrapper');
+					var href = $(this).attr('href');
 
-		$wrappers
-			.each(function() {
+					event.preventDefault();
+					event.stopPropagation();
 
-				var $this = $(this),
-					on, off;
+					// Hide.
+						$menu._hide();
 
-				on = function() {
+					// Redirect.
+						window.setTimeout(function() {
+							window.location.href = href;
+						}, 350);
 
-					$this.scrollex({
-						top:		250,
-						bottom:		0,
-						initialize:	function(t) { $this.addClass('inactive'); },
-						terminate:	function(t) { $this.removeClass('inactive'); },
-						enter:		function(t) { $this.removeClass('inactive'); },
+				});
 
-						// Uncomment the line below to "rewind" when this wrapper scrolls out of view.
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
 
-						//leave:	function(t) { $this.addClass('inactive'); },
+				event.stopPropagation();
+				event.preventDefault();
 
-					});
+				// Toggle.
+					$menu._toggle();
 
-				};
+			})
+			.on('keydown', function(event) {
 
-				off = function() {
-					$this.unscrollex();
-				};
-
-				breakpoints.on('<=medium', off);
-				breakpoints.on('>medium', on);
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
 
 			});
-
-	// Banner.
-		var $banner = $('#banner');
-
-		$banner
-			._parallax();
 
 })(jQuery);
