313064a052e36b8bff3a5ff540262354395f3972
brellin <cekakid@gmail.com>
2020-02-13 17:04:16 -0600

    add github repositories

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -12,21 +12,39 @@ const Work = props => {
         <div className="Project BlackHole">
             <p onClick={() => window.open('https://blackhole.willujr.com')}>{`A web app that allows the user to create an account, log in, and vent about anything that has been troubling you - without the worry of anybody finding out about what you said.
             
-            I was responsible for the front end of this project.`}</p>
+            I was responsible for the front end of this project.
+            
+            `}<a
+                    href="https://github.com/black-hole-lambda-build-week/front-end"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >GitHub Repository</a></p>
         </div>
 
         <h2>Howl For Change</h2>
         <div className="Project Howl">
             <p onClick={() => window.open('https://howl-for-change-staging.netlify.com')}>{`A marketplace that allows nonprofit organizations to post projects for creative users to apply for - using portfolios made within the app. Only one 'winner' is chosen per project.
             
-            I was the lead technical developer for this project.`}</p>
+            I was the lead technical developer for this project.
+            
+            `}<a
+                    href="https://bitbucket.org/hfc-wolfpack/howlforchange/"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >Bitbucket Repository</a></p>
         </div>
 
         <h2>Chao Fever</h2>
         <div className="Project Chao">
-            <p onClick={() => window.open('https://blackhole.willujr.com')}>{`My first maserpiece, made at 15 years of age - 2008
+            <p on
+                Click={() => window.open('https://blackhole.willujr.com')}
+                target='_blank'
+                rel='noopener noreferrer'
+            >{`My first maserpiece, made at 15 years of age - 2008
             
-            The website is focused on a side-story of the Sonic Adventure series...Chao!`}</p>
+            The website is focused on a side-story of the Sonic Adventure series...Chao!
+            
+            `}<a href="https://github.com/brellin/chaofever">GitHub Repository</a></p>
         </div>
     </div>
 
