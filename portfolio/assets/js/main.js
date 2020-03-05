0531df437b71abb60f864da6a9a7b3fac21b3977
Yeshi <tyeshi777@yahoo.com>
2020-03-05 07:25:28 -0800

    added button for details

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -5,30 +5,35 @@
 */
 
 (function($) {
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"]
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function() {
+    window.setTimeout(function() {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
-
+  // Scrolly.
+  $("#nav a, .scrolly").scrolly({
+    speed: 1000,
+    offset: function() {
+      return $nav.height();
+    }
+  });
 })(jQuery);
+
+let button = document.getElementById("details");
+button.addEventListener("click", function() {
+  console.log("clicked");
+});
