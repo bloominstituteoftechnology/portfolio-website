344bfd4a4e84c8176d6060ac3d481b91a2f1a8c2
unknown <wastefuldragon@gmail.com>
2020-04-12 00:48:02 -0400

    style changes

--- assets/js/user-profile.js
+++ assets/js/user-profile.js
@@ -1,3 +1,9 @@
+//doesn't worrk locally will have to test once live to see if thats issue
 function goBack() {
   window.history.back();
 }
+
+var card = document.querySelector('.pie');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
