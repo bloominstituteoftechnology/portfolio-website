cc493cad7b955411bdbb31b4a398c7d9983aaf5c
Devin Warrick <49497246+DevWarr@users.noreply.github.com>
2021-05-08 15:10:57 -0500

    Updates form error to include response.error from formspree (#9)

--- assets/js/formspree.js
+++ assets/js/formspree.js
@@ -2,46 +2,47 @@ let form = document.getElementById('contact-form');
 let sending = false;
 let statusMessage = document.getElementById('contact-form-status');
 
-function resetForm() {
-	statusMessage.textContent = ' ';
-	sending = false;
+function resetErrorMessage() {
+    statusMessage.textContent = ' ';
+    sending = false;
 }
 
-function resetFormAndRedirect() {
-	resetForm();
-	const url = window.location.href;
-	window.location.href = url.replace('#contact', '#');
+function resetErrorMessageAndRedirect() {
+    resetErrorMessage();
+    const url = window.location.href;
+    window.location.href = url.replace('#contact', '#');
 }
 
-function handleResponse(status) {
-	if (200 <= status && status < 300) {
-		statusMessage.textContent = 'Thank you! Message sent.';
-		setTimeout(resetFormAndRedirect, 2000);
-	} else if (400 <= status && status < 500) {
-		statusMessage.textContent = "Uh oh! There's been an error. Please try again.";
-		setTimeout(resetForm, 2000);
-	} else {
-		statusMessage.textContent = "There's a problem with the servers. Try again later.";
-		setTimeout(resetForm, 2000);
-	}
+function handleResponse(status, response) {
+    if (200 <= status && status < 300) {
+        statusMessage.textContent = 'Thank you! Message sent.';
+        setTimeout(resetErrorMessageAndRedirect, 2000);
+    } else if (400 <= status && status < 500) {
+        errorMessage = response.error || 'There was an error sending the form';
+        statusMessage.textContent = `Uh oh! ${errorMessage}. Please try again.`;
+        setTimeout(resetErrorMessage, 2000);
+    } else {
+        statusMessage.textContent = "There's a problem with the servers. Try again later.";
+        setTimeout(resetErrorMessage, 2000);
+    }
 }
 
 form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	if (!sending) {
-		sending = true;
-		const data = new FormData(form);
-		ajax(form.method, form.action, data, handleResponse);
-	}
+    e.preventDefault();
+    if (!sending) {
+        sending = true;
+        const data = new FormData(form);
+        ajax(form.method, form.action, data, handleResponse);
+    }
 });
 
 function ajax(method, url, data, handleResponse) {
-	const xhr = new XMLHttpRequest();
-	xhr.open(method, url);
-	xhr.setRequestHeader('Accept', 'application/json');
-	xhr.onreadystatechange = () => {
-		if (xhr.readyState !== XMLHttpRequest.DONE) return;
-		handleResponse(xhr.status, xhr.response, xhr.responseType);
-	};
-	xhr.send(data);
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        handleResponse(xhr.status, xhr.response, xhr.responseType);
+    };
+    xhr.send(data);
 }
